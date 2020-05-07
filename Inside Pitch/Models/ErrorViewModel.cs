9ffb9469058ee785a89a62daf2c740655885d695
shelbyj <shelbyj@DESKTOP-E3G10IS>
2020-05-07 17:35:32 -0500

    Add project files.

---
+++ Inside Pitch/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Inside_Pitch.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
