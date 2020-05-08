ac9f258990cb9839cdd22215aed0a2c5450c7705
shelbyj <shelbyj@DESKTOP-E3G10IS>
2020-05-08 11:32:54 -0500

    Another Test Commit of changes ive made, I've added registerviewmodel, register, and accountcontroller, to make the page where users can register, but they currently do not work, i get an error 404

---
+++ Inside Pitch/Models/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Inside_Pitch.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
